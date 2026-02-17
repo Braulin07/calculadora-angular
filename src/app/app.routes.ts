cb57c32ae87a1cd55cc28b3e28ff45b858888f1e
braulin07 <braulinpg@gmail.com>
2026-02-17 10:57:57 -0400

    configuration of routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
+import { CalculadoraComponent } from './pages/calculadora/calculadora';
+import { AboutComponent } from './pages/about/about';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'calculadora', component: CalculadoraComponent },
+  { path: 'about', component: AboutComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
